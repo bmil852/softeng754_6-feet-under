06a7dc2f19666ddbb58b938bc9f9f1bc9508cdee
Brad Miller <bmil852@aucklanduni.ac.nz>
2018-05-17 14:53:07 +1200

    Add unimplemented stepdefs for login functions - Story 1

---
+++ src/test/java/nz/ac/auckland/stepdefinitions/LoginStepDefs.java
@@ -0,0 +1,166 @@
+package nz.ac.auckland.stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepDefs {
+	
+	@Given("^a new User wants to sign up to the system$")
+	public void a_new_User_wants_to_sign_up_to_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the User provides valid details for signing up$")
+	public void the_User_provides_valid_details_for_signing_up() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the User is succesfully registered with the system$")
+	public void the_User_is_succesfully_registered_with_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^a new Administrator wants to sign up to the system$")
+	public void a_new_Administrator_wants_to_sign_up_to_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the Administrator provides valid details for signing up$")
+	public void the_Administrator_provides_valid_details_for_signing_up() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the Administrator is succesfully registered with the system$")
+	public void the_Administrator_is_succesfully_registered_with_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the User provides invalid details for signing up$")
+	public void the_User_provides_invalid_details_for_signing_up() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the User is not succesfully registered with the system$")
+	public void the_User_is_not_succesfully_registered_with_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the Administrator provides invalid details for signing up$")
+	public void the_Administrator_provides_invalid_details_for_signing_up() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the Administrator is not succesfully registered with the system$")
+	public void the_Administrator_is_not_succesfully_registered_with_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^a User exists in the system$")
+	public void a_User_exists_in_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the User provides valid details for signing in$")
+	public void the_User_provides_valid_details_for_signing_in() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the User is succesfully signed in$")
+	public void the_User_is_succesfully_signed_in() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^an Administrator exists in the system$")
+	public void an_Administrator_exists_in_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the Administrator provides valid details for signing in$")
+	public void the_Administrator_provides_valid_details_for_signing_in() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the Administrator is succesfully signed in$")
+	public void the_Administrator_is_succesfully_signed_in() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the User provides invalid details for signing in$")
+	public void the_User_provides_invalid_details_for_signing_in() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the User is not succesfully signed in$")
+	public void the_User_is_not_succesfully_signed_in() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the Administrator provides invalid details for signing in$")
+	public void the_Administrator_provides_invalid_details_for_signing_in() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the Administrator is not succesfully signed in$")
+	public void the_Administrator_is_not_succesfully_signed_in() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^a User is already signed into the system$")
+	public void a_User_is_already_signed_into_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the User signs out of the system$")
+	public void the_User_signs_out_of_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the User is no longer signed in to the system$")
+	public void the_User_is_no_longer_signed_in_to_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^an Administrator is already signed into the system$")
+	public void an_Administrator_is_already_signed_into_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the Administrator signs out of the system$")
+	public void the_Administrator_signs_out_of_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the Administrator is no longer signed in to the system$")
+	public void the_Administrator_is_no_longer_signed_in_to_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+}
