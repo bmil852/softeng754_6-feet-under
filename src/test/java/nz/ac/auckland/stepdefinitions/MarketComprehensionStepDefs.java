7389a6789bced683e1fd90a1eb97422718f5df2f
Brad Miller <bmil852@aucklanduni.ac.nz>
2018-05-19 01:48:48 +1200

    Add unimplemented stepdefs for document return - Story 7

---
+++ src/test/java/nz/ac/auckland/stepdefinitions/MarketComprehensionStepDefs.java
@@ -0,0 +1,28 @@
+package nz.ac.auckland.stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MarketComprehensionStepDefs {
+
+	@Given("^a User has a set of weighted keywords to search for$")
+	public void a_User_has_a_set_of_weighted_keywords_to_search_for() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the User performs a search$")
+	public void the_User_performs_a_search() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^a non-empty set of documents will be returned$")
+	public void a_non_empty_set_of_documents_will_be_returned() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+}
