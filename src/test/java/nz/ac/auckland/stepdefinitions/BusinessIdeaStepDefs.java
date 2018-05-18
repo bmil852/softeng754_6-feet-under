cc33077d0f88668f15dbd96fcf19022d9c1bbd91
Brad Miller <bmil852@aucklanduni.ac.nz>
2018-05-18 20:03:07 +1200

    Add unimplemented stepdefs for keyword extraction functions - Story 4

---
+++ src/test/java/nz/ac/auckland/stepdefinitions/BusinessIdeaStepDefs.java
@@ -0,0 +1,28 @@
+package nz.ac.auckland.stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class BusinessIdeaStepDefs {
+	
+	@Given("^the User wants to input their business idea to the system$")
+	public void the_User_wants_to_input_their_business_idea_to_the_system() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the User enters their business idea as \"([^\"]*)\"$")
+	public void the_User_enters_their_business_idea_as(String arg1) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the System will extract (no|a number of) keywords from the business idea$")
+	public void the_System_will_extract_no_keywords_from_the_business_idea() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+}
