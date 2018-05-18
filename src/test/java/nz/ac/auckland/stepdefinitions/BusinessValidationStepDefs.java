3289127bd664538f37b62ad542b7f48c4e5c5ea0
Brad Miller <bmil852@aucklanduni.ac.nz>
2018-05-19 05:04:19 +1200

    Add unimplemented stepdefs for category popularity - Story 11

---
+++ src/test/java/nz/ac/auckland/stepdefinitions/BusinessValidationStepDefs.java
@@ -0,0 +1,34 @@
+package nz.ac.auckland.stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class BusinessValidationStepDefs {
+
+	@Given("^the search results have returned an example set of documents as described$")
+	public void the_search_results_have_returned_an_example_set_of_documents_as_described() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^the popularity of each category is processed$")
+	public void the_popularity_of_each_category_is_processed() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^category (\\d+) should have a popularity of (\\d+) percent$")
+	public void category_should_have_a_popularity_of_percent(int arg1, int arg2) throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^an additional fourth category with no documents exists$")
+	public void an_additional_fourth_category_with_no_documents_exists() throws Exception {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+}
